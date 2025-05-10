8808fbad98fcca4758bc97f5acadb86bbd665088
Julian Fuemmeler <juliankane@gmail.com>
2025-05-10 15:43:07 -0500

    Delete cognito.config.js

--- cognito.config.js
+++
@@ -1,9 +0,0 @@
-import { CognitoUserPool } from 'amazon-cognito-identity-js';
-
-
-const poolData = {
-    UserPoolId: 'us-east-1_E5QqPg4Pi',
-    ClientId: '3ngi33a8p005nb4hsr84nmva0c',
-};
-  
-export default new CognitoUserPool(poolData);
